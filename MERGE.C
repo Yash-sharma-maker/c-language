77cb6bf7fbe8b8b2a8ac3d640198d1ed51aec3ae
yash sharma <54267949+Yash-sharma-maker@users.noreply.github.com>
2019-10-01 17:39:32 +0530

    Add files via upload

---
+++ MERGE.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a,b;
+printf("enter the  two no");
+scanf("%d",&a,&b);
+n=a;
+a=b;
+b=n;
+printf("the swapping of two no is %d%d");
+getch();
+}
+
